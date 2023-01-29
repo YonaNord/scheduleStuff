21402d4191c495d21ecb160cf1eaa7c222f621a8
JoniNordi <52534955+YonaNord@users.noreply.github.com>
2023-01-29 09:22:22 +0200

    Organizing

---
+++ React/schedule-stuff/src/_codux/boards/new-board.board.tsx
@@ -0,0 +1,11 @@
+import { createBoard } from '@wixc3/react-board';
+
+export default createBoard({
+    name: 'New Board',
+    Board: () => <div>
+        <h1>Heading 1</h1>
+    </div>,
+    environmentProps: {
+        windowWidth: 848
+    }
+});
